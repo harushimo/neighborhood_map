2e70daceb31063626543e56d6aabed4c0421a9e0
harushimo <hvedam@gmail.com>
2017-04-10 23:52:11 -0500

    removed marker from the map until map stretched out

--- js/app.js
+++ js/app.js
@@ -4,11 +4,7 @@
 function initMap() {
   var chicago = {lat: 41.8333908, lng: -88.0130296}
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 10,
     center: chicago
-  });
-  var marker = new google.maps.Marker({
-    position: chicago,
-    map: map
   });
 }
